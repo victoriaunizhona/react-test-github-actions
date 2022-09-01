8e6a55432773b7933d4d818b09f12c34fc08cf94
Victoria Unizhona <vikaunizhona@gmail.com>
2022-09-01 21:02:47 +0300

    fix homepage link

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit
           <code>src/App.js</code>
-          and save to reload.
+          and save to reload the page.
         </p>
         <a
           className="App-link"
